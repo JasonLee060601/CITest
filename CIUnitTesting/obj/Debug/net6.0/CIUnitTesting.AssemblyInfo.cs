9c47f6b1d61ed693ddd727bd666d80318cb407db
Jason BrainlessJason <brainlessjason@gmail.com>
2025-03-18 14:37:01 +0800

    commit

--- CIUnitTesting/obj/Debug/net6.0/CIUnitTesting.AssemblyInfo.cs
+++ CIUnitTesting/obj/Debug/net6.0/CIUnitTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIUnitTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54758105f6598a7acb32b51a0987a262d795f2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIUnitTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIUnitTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
